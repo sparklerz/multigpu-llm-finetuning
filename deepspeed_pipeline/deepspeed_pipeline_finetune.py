1021ab7e05c0f1d6c8021e8f941d7c6aa02940d1
sarat kannan <kannansarat9@gmail.com>
2025-06-07 13:26:43 +0530

    Revert "modify config to fix oom"
    This reverts commit ee15b5ae82277725cc1ecb7a9bad44d487c00326.

--- deepspeed_pipeline/deepspeed_pipeline_finetune.py
+++ deepspeed_pipeline/deepspeed_pipeline_finetune.py
@@ -397,8 +397,8 @@ def main():
             "allgather_bucket_size": 2e7,
             "reduce_scatter": False,
             "reduce_bucket_size": 2e7,
-            "overlap_comm": True,
-            "contiguous_gradients": True,
+            "overlap_comm": False,
+            "contiguous_gradients": False,
             "offload_optimizer": {
                 "device": "none"
             }
