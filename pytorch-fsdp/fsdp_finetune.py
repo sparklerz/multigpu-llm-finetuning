fix gradient checkpointing

pytorch-fsdp/fsdp_finetune.py
@@ -88,7 +88,7 @@ class Trainer:
 
         # Initialize model with FSDP
         model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.float16)
-        model.gradient_checkpointing_enable()
+        model.gradient_checkpointing_enable(use_reentrant=False)
         fsdp_model = FSDP(
             model,
             mixed_precision=MixedPrecision(
@@ -96,14 +96,14 @@ class Trainer:
                 reduce_dtype=torch.float16,
                 buffer_dtype=torch.float16,
             ),
-            forward_prefetch=True,
+            forward_prefetch=False,
             limit_all_gathers=True,
             device_id=self.local_rank
         )
         self.model = fsdp_model.to(self.device)
 
         # Optimizer and GradScaler for AMP
-        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=5e-6, fused=True)
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=5e-6)
         self.scaler = torch.cuda.amp.GradScaler()
 
         # Compute total steps